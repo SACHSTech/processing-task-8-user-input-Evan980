8223e4ae06c7e7a14da5274420799934de31b17d
Evan Chan <100213223+Evan980@users.noreply.github.com>
2022-05-03 03:40:57 +0000

    almost done

--- Sketch.java
+++ Sketch.java
@@ -6,9 +6,17 @@ public class Sketch extends PApplet {
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
+
+  // Variables
+  boolean leftPressed;
+  boolean rightPressed;
+  int intHouseX;
+  int intHouseY;
+  int intScale;
+  
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -22,15 +30,67 @@ public class Sketch extends PApplet {
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  public void draw(){
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    if(keyPressed){
+      if(key == 'q'){
+        background(0, 0, 0);
+      }
+      else if(key == 'w'){
+        background(255, 0, 0);
+      }
+      else if(key == 'e'){
+        background(0, 255, 0);
+      }
+      else if(key == 'r'){
+        background(0, 0, 255);
+      }
+    }
+
+    if(mousePressed){
+    // body of house
+    fill(0, 0, 0);
+    rect(intHouseX, intHouseY+(intScale/8), (intScale/2), (intScale/2));
+
+    // roof
+    fill(0, 255, 0);
+    triangle(intHouseX, intHouseY+(intScale/8), intHouseX+(intScale/4), intHouseY, intHouseX+(intScale/2), intHouseY+(intScale/8));
+
+    // door
+    fill(170, 80, 0);
+    rect(intHouseX+(intScale/4), intHouseY+((intScale/4)+(intScale/8)), (intScale/8), (intScale/4));
   }
-  
-  // define other methods down here.
+ } 
+
+    
+  // Draw dots when mouse clicks
+  public void mouseClicked(){
+    fill(50, 200, 100);
+    ellipse(mouseX, mouseY, 10, 10);
+  }
+  // Detects if key is pressed
+  public void keyPressed(){
+    if(keyCode == LEFT){
+      leftPressed = true;
+    }
+    if(keyCode == RIGHT){
+      rightPressed = true;
+    }
+  }
+
+  // Detects key released
+  public void keyReleased(){
+    if(keyCode == LEFT){
+      leftPressed = false;
+    }
+    if(keyCode == RIGHT){
+      rightPressed = false;
+    }
+  }
+
+// Detects mouse pressed
+  public void mousePressed(){
+    fill(255, 255, 255);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
 }
